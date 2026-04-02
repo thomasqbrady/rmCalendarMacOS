bcfcf858698847fa9769b7e84e168ee252f9dd4a
Thomas Q Brady <tqb@thomasqbrady.com>
2026-04-02 14:57:51 -0400

    Update formula SHA for v0.1.25

--- Formula/rmcal.rb
+++ Formula/rmcal.rb
@@ -3,8 +3,8 @@ class Rmcal < Formula
 
   desc "Sync macOS Calendar to reMarkable tablets as interactive PDF planners"
   homepage "https://github.com/thomasqbrady/rmCalendarMacOS"
-  url "https://github.com/thomasqbrady/rmCalendarMacOS/archive/refs/tags/v0.1.24.tar.gz"
-  sha256 "161db7dc17072a2ab4c65b96ca994863a313dd4aefb480e2f825a7f9b6712abe"
+  url "https://github.com/thomasqbrady/rmCalendarMacOS/archive/refs/tags/v0.1.25.tar.gz"
+  sha256 "a2b7baf0429263e3ec711822ad65f270f02195cae3eb3316f8825ca85b163db3"
   license "MIT"
   head "https://github.com/thomasqbrady/rmCalendarMacOS.git", branch: "main"
 
